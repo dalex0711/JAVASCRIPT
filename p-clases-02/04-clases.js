97a4973f9929b89ad929d96347eb4e2223638841
Dalex <dalexasanjuan@outlook.com>
2025-07-01 10:48:00 -0500

    ejercicio de clase

--- p-clases-02/04-clases.js
+++ p-clases-02/04-clases.js
@@ -1,10 +1,35 @@
 class Producto {
     constructor(name,precio){
-        this.nombre = nombre;
+        this.name = name;
         this.precio = precio;
     }
 
     formatearProducto(){
-        return`El prodcuto`
+        return`El producto ${this.name} tiene un precio de: $${this.precio}`
+    }
+
+    obtenerPrecio(){
+        return `El precio del producto ${this.name} es ${this.precio}`
     }
 }
+
+// const producto2 = new Producto('pan',1000);
+// const producto3 = new Producto('azucar',1000);
+// console.log(producto2.obtenerPrecio())
+
+class Libro extends  Producto {
+    constructor(name,precio,codigo){
+        super(name,precio);
+        this.codigo = codigo;
+    }
+    formatearProducto(){
+        return`${super.formatearProducto()} y su codigo es ${this.codigo}`
+    }
+
+
+}
+
+
+const libro = new Libro('Java',120)
+
+console.log(libro.formatearProducto())
